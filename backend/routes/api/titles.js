891738c31aea61f056abdaca7d4844d834c8581b
rijulradhu <rijulradhu12@gmail.com>
2022-08-02 18:04:09 +0530

    Issue Fix try6

--- backend/routes/api/titles.js
+++ backend/routes/api/titles.js
@@ -5,9 +5,8 @@ var Item = mongoose.model("Item");
 // return a list of titles
 router.get("/", function (req, res, next) {
   Item.findOne({ title: req.query.title })
-    .then((titles) => {
-      console.log(titles);
-      return res.json({ title: titles });
+    .then(() => {
+      return res.json();
     })
     .catch(next);
 });
